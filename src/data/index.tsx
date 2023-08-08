c27396678e7a1ebe194734a72b1daed0df4a674a
aldo <github@aldo.io>
2023-08-08 12:38:34 +1000

    answer question 3

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
+import Link from "next/link";
 import { isQuestion } from "../types";
+import Image from "next/image";
 
 export const questions: isQuestion[] = [
   {
@@ -184,7 +186,248 @@ export const questions: isQuestion[] = [
     question:
       "How would you compare Next.js with another frontend framework? Feel free to compare with a framework of your choice.",
     summary: "Comparing Next.js with other frameworks",
-    answer: <p>Answer content 3</p>,
+    answer: (
+      <>
+        <h3>Next.js vs SvelteKit</h3>
+        Considering Vercel's significant role in the development of both of
+        these technologies, it seemed appropriate to compare{" "}
+        <code>Next.js</code> with <code>SvelteKit</code>.
+        <h3>
+          I'd like some <strike>cheese with my pasta</strike> pasta with my
+          cheese
+        </h3>
+        <p>
+          Back in the day I loved making static sites with <code>Jekyll</code>.
+          Writing templates in <code>Liquid</code> or <code>Nunjucks</code> felt
+          efficient and intuitive. These projects had very little dynamic
+          functionality and it was almost always enough to simply sprinkle some
+          JS on top to get the job done.
+        </p>
+        <p>
+          Today, working with <code>SvelteKit</code> feels very similar to my
+          SSG / Jamstack days. I usually start writing components HTML first,
+          sometimes not even adding <code>script</code> tags for simple pieces.
+        </p>
+        <p>
+          This is quite a contrast from <code>Next.js</code> where the first
+          thing I do is generate components using snippet extensions or JS
+          patterns from memory. It takes a little more effort to get to writing
+          JSX.
+        </p>
+        <p>
+          There's no right or wrong way to do this. For developers like myself
+          who started their career in the front-of-the-frontend<b>*</b>, a
+          markup first approach can feel more natural. For complex webapps,
+          there's a lot of benefit to writing JS first and maintaining
+          consistency in well established <code>React</code> patterns.
+        </p>
+        <p className="text-xs text-gray-500">
+          <b>*</b> See{" "}
+          <Link href="https://css-tricks.com/the-great-divide/" target="_blank">
+            The Great Divide
+          </Link>{" "}
+          from CSS Tricks for a brilliant discussion on this topic.
+        </p>
+        <h3>Big fish, fast fish</h3>
+        <p>
+          <code>Next.js</code> is a big fish. Building on the existing
+          popularity and scale of <code>React</code> ecosystem its only natural
+          that <code>Next.js</code> would be the market leader for fully fledged
+          application boilerplates and frameworks.
+        </p>
+        <p>
+          This scale has many benefits:
+          <ul>
+            <li>Large amounts of high quality documentation and tutorials</li>
+            <li>Expansive plugin ecosystem</li>
+            <li>Strong talent pool of experienced developers</li>
+            <li>
+              Higher degree of trust, allowing for smoother stakeholder approval
+            </li>
+          </ul>
+        </p>
+        <p>
+          However this can also have drawbacks:
+          <ul>
+            <li>
+              Bloat
+              <ul>
+                <li>
+                  Considering how many millions of developers rely on the tool,
+                  maintaining legacy functionality is incredibly important.
+                  Additionally the volume of feature requests could contribute
+                  to larger and larger package sizes.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Performance
+              <ul>
+                <li>
+                  On a related note, due to the complexity of larger projects;
+                  performance can be a bigger challenge. More dependencies to
+                  worry about, more tech debt to work through before performance
+                  gains can be realized.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Innovator's dilemma
+              <ul>
+                <li>
+                  Large projects need to be slow and cautious in adopting new
+                  patterns or technologies. This can lead to a slower pace of
+                  innovation and could lead to decisions between an existing
+                  abstraction or adopting new native browser features. For
+                  example, virtual DOM vs native DOM.
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </p>
+        <h3>Nuts and bolts</h3>
+        <p>
+          Getting into more specific details, there are some small yet
+          significant differences between <code>Next.js</code> and{" "}
+          <code>SvelteKit</code> from a technical perspective
+        </p>
+        <h4>Hot reloading</h4>
+        <p>
+          <code>SvelteKit</code> uses <code>Vite</code> out of the box which has
+          hot reloading built in. While you can achieve this in{" "}
+          <code>Next.js</code> with <code>Turbopack</code>, it isn't enabled by
+          default
+        </p>
+        <h4>Weight</h4>
+        <p>
+          <code>SvelteKit</code> is a significantly smaller package taking up
+          approximately 17% of the space <code>Next.js</code> requires.
+        </p>
+        <h4>OOTB forms, state management and animations</h4>
+        <p>
+          While there are equivalents for each of these features in the{" "}
+          <code>Next.js</code> ecosystem (eg. <code>Zustand</code> or{" "}
+          <code>Formik</code>), these are out-of-the-box in{" "}
+          <code>SvelteKit</code>.
+        </p>
+        <h4>OOTB image handling</h4>
+        <p>
+          <code>Next.js</code> wins this one thanks to the{" "}
+          <code>next/image</code> feature.
+        </p>
+        <h4>Accessibility hints</h4>
+        <p>
+          <code>SvelteKit</code> has a built in A11y console hints. This is
+          minor though considering that there are many well established{" "}
+          <code>eslint</code> configs to handle this on the <code>Next.js</code>{" "}
+          side.
+        </p>
+        <h3>Conclusion</h3>
+        <p>
+          Developers love to argue about their tools and honestly I think that's
+          a good thing. It exposes us to new ideas and potentially sheds light
+          on flaws in our current approaches.
+        </p>
+        <Image
+          src={"/wow-fuck-that-guy.jpg"}
+          alt={
+            "the famous 'wow fuck that guy' comic where a guy finds a flag on the ground"
+          }
+          width={922}
+          height={1390}
+          className="w-full"
+        />
+        <p className="text-xs text-gray-500">
+          Source{" "}
+          <Link
+            href="https://www.extrafabulouscomics.com/archive"
+            target="_blank"
+          >
+            Extra Fabulous Comics
+          </Link>
+        </p>
+        <p>
+          You can build wonderful applications with each of these technologies
+          and it really comes down to personal preference. Use the tool that
+          you're most productive with because at the end of the day our users
+          only care about having a great UX.
+        </p>
+        <hr />
+        <h3>Additional notes</h3>
+        <h4>Frameworks vs meta-frameworks</h4>
+        <p>
+          The word <code>framework</code> can feel like a tricky thing to
+          define. When does a project go from a tool or library to a framework?
+        </p>
+        <p>
+          While we commonly hear technologies like <code>React</code> or{" "}
+          <code>Svelte</code> referred to as frameworks, they could be
+          classified as UI libraries considering the scope of their
+          functionality often ends when it comes to server side, routing or even
+          SEO best practice.
+        </p>
+        <p>
+          So what do we call the tools like <code>Next.js</code> that handle it
+          all? A more accurate term might be <code>meta-framework</code>.
+        </p>
+        <h4>The big four and their meta counterparts</h4>
+        <p>
+          Based on{" "}
+          <Link
+            href="https://npmtrends.com/angular-vs-react-vs-svelte-vs-vue"
+            target="_blank"
+          >
+            npm trends
+          </Link>
+          ,{" "}
+          <Link
+            href="https://survey.stackoverflow.co/2023/#web-frameworks-and-technologies"
+            target="_blank"
+          >
+            Stackoverflow's 2023 Developer Survey
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="https://2022.stateofjs.com/en-US/libraries/front-end-frameworks/#front_end_frameworks_experience_linechart"
+            target="_blank"
+          >
+            State of JS 2022
+          </Link>{" "}
+          the following frameworks continue to be the most popular.
+        </p>
+        <ul>
+          <li>
+            <code>React</code> 👉 <code>Next</code>
+          </li>
+          <li>
+            <code>Angular</code> <b>*</b>
+          </li>
+          <li>
+            <code>Vue</code> 👉 <code>Nuxt</code>
+          </li>
+          <li>
+            <code>Svelte</code> 👉 <code>SvelteKit</code>
+          </li>
+        </ul>
+        <p className="text-xs text-gray-500">
+          <b>*</b> Angular is already a meta-framework, especially considering
+          things like{" "}
+          <Link href="https://angular.io/guide/universal" target="_blank">
+            Angular Universal
+          </Link>
+        </p>
+        <p>
+          Its worth noting that <code>Preact</code> has noteworthy popularity
+          but I consider it closely related to the <code>React</code> ecosystem
+          considering their shared API.
+        </p>
+        <p>
+          Additionally, <code>Angular</code> popularity continues to decline
+          although based on its significant enterprise usage and backing by
+          Google I'm sure it will remain an in-demand technology.
+        </p>
+      </>
+    ),
   },
   {
     id: 4,
