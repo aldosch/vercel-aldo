52a92c83e48e15b33ca4273ac36bd95b27012772
aldo <github@aldo.io>
2023-08-08 12:40:31 +1000

    adjust badge position

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -10,12 +10,14 @@ export default function QuestionCard(props: Props) {
   const item = questions[props.id - 1];
   return (
     <>
+      <div className="flex justify-end my-4">
+        <span className="items-center justify-end px-2 py-1 mb-2 text-xs font-medium text-green-700 bg-green-100 rounded-full">
+          Question {item?.id}
+        </span>
+      </div>
       <div className="border shadow rounded-xl w-fit">
         <div className="prose prose-code:bg-gray-100 prose-code:rounded prose-code:p-1 prose-code:text-gray-800 prose-code:font-normal prose-blockquote:">
           <div className="px-4 py-5 border-b border-gray-200 sm:px-6">
-            <span className="inline-flex items-center px-2 py-1 mb-2 text-xs font-medium text-green-700 bg-green-100 rounded-full">
-              Question {item?.id}
-            </span>
             <h1 className="mb-0 text-xl font-semibold text-gray-900">
               {item?.question}
             </h1>
